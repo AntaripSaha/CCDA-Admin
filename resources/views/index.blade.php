d7da7b909cc5000d8b7f308456698679a516ae5f
AntaripSaha <antarip15@gmail.com>
2022-06-14 17:09:08 +0600

    layouts

---
+++ resources/views/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admin')
+@section('content')
+
+
+<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+	
+
+	<!--begin::Content-->
+	<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+		<!--begin::Toolbar-->
+		<div class="toolbar" id="kt_toolbar">
+			<!--begin::Container-->
+			<div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
+				<!--begin::Page title-->
+				<div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+					<!--begin::Title-->
+					<h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Dashboard
+					<!--begin::Separator-->
+						<span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
+					</h1>
+					<!--end::Title-->
+				</div>
+				<!--end::Page title-->
+		
+			</div>
+			<!--end::Container-->
+		</div>
+		<!--end::Toolbar-->
+
+	</div>
+	<!--end::Content-->
+	<!--begin::Footer-->
+	<div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+		<!--begin::Container-->
+		<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+			<!--begin::Copyright-->
+			<div class="text-dark order-2 order-md-1">
+				<span class="text-muted fw-bold me-1">2022©</span>
+				<a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+			</div>
+			<!--end::Copyright-->
+			<!--begin::Menu-->
+			<ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
+				<li class="menu-item">
+					<a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
+				</li>
+				<li class="menu-item">
+					<a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
+				</li>
+				<li class="menu-item">
+					<a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
+				</li>
+			</ul>
+			<!--end::Menu-->
+		</div>
+		<!--end::Container-->
+	</div>
+	<!--end::Footer-->
+</div>
+				
+
+
+
+@endsection
